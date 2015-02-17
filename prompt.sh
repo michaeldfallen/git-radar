8617edf7a1cd6cc8bcdc9ec64d9e11faa28221c2
Michael Allen <michael@michaelallen.io>
2015-02-17 10:58:04 +0000

    an example prompt built using the git-base functions

---
+++ prompt.sh
@@ -0,0 +1,23 @@
+source ./git-base.sh
+
+local_ahead="$(commits_ahead_of_remote)"
+ahead_arrow="↑"
+
+if [[ ("$local_ahead" -gt 0) ]]; then
+  local_ahead=" ${local_ahead}${ahead_arrow}"
+else
+  local_ahead=""
+fi
+
+local_behind="$(commits_behind_of_remote)"
+behind_arrow="↓"
+
+if [[ "$local_behind" -gt "0" ]]; then
+  local_behind=" ${local_behind}${behind_arrow}"
+else
+  local_behind=""
+fi
+
+prompt="$(branch_name)$local_ahead$local_behind"
+
+echo $prompt
