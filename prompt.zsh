5bfe5141cce192b17d1327809126048ec6dc622c
Michael Allen <michael@michaelallen.io>
2015-08-19 09:47:25 +0100

    Color zsh prompt slightly differently

--- prompt.zsh
+++ prompt.zsh
@@ -5,12 +5,12 @@ source "$dot/git-base.sh"
 
 if is_repo; then
   autoload colors && colors
-  git_prefix="%{$fg_bold[black]%} git:(%{$reset_color%}"
-  git_suffix="%{$fg_bold[black]%})%{$reset_color%}"
-  printf '%s' $git_prefix
+  printf '%s' "%{$fg_bold[black]%} git:(%{$reset_color%}"
   zsh_color_remote_commits
-  branch_name
+  printf '%s' "%{$fg[white]%}"
+  readable_branch_name
+  printf '%s' "%{$reset_color%}"
   zsh_color_local_commits
-  printf '%s' $git_suffix
+  printf '%s' "%{$fg_bold[black]%})%{$reset_color%}"
   zsh_color_changes_status
 fi
