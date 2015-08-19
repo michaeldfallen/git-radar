72fca3c9a52dd8b8c2970c43e7cbee1b94f2f707
Michael Allen <michael@michaelallen.io>
2015-08-19 09:35:16 +0100

    Reduce the number of subshells, for performance

--- prompt.zsh
+++ prompt.zsh
@@ -5,7 +5,12 @@ source "$dot/git-base.sh"
 
 if is_repo; then
   autoload colors && colors
-  git_prefix="%{$fg_bold[black]%}git:(%{$reset_color%}"
+  git_prefix="%{$fg_bold[black]%} git:(%{$reset_color%}"
   git_suffix="%{$fg_bold[black]%})%{$reset_color%}"
-  printf '%s' " $git_prefix$(zsh_color_remote_commits;branch_name;zsh_color_local_commits)$git_suffix$(zsh_color_changes_status)"
+  printf '%s' $git_prefix
+  zsh_color_remote_commits
+  branch_name
+  zsh_color_local_commits
+  printf '%s' $git_suffix
+  zsh_color_changes_status
 fi
