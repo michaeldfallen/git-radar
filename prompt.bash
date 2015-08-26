637c573f7324538f27cd8a6869193e1364d75e76
Grigory Petrov <grigory.v.p@gmail.com>
2015-08-26 14:37:43 +0300

    Revert "bash prompt length fix: zero-length chars must be placed between \[ and \]"
    This reverts commit 0fe65ed830b5d1cdc5189230139b52f4c8aacfbc.

--- prompt.bash
+++ prompt.bash
@@ -4,12 +4,12 @@ dot="$(cd "$(dirname "$0")"; pwd)"
 source "$dot/radar-base.sh"
 
 if is_repo; then
-  printf " \[\033[1;30m\]git:(\[\033[0m\]"
+  printf " \033[1;30mgit:(\033[0m"
   bash_color_remote_commits
-  printf "\[\033[0;37m\]"
+  printf "\033[0;37m"
   readable_branch_name
-  printf "\[\033[0m\]"
+  printf "\033[0m"
   bash_color_local_commits
-  printf "\[\033[1;30m\])\[\033[0m\]"
+  printf "\033[1;30m)\033[0m"
   bash_color_changes_status
 fi
