c03a462177bc2ec469a0cdb06234d08f35054966
Reinaldo Colina <rcolina@pingidentity.com>
2015-08-28 20:23:06 -0700

    Added change for issue-17 to prompt.zsh as well

--- prompt.zsh
+++ prompt.zsh
@@ -1,12 +1,15 @@
 #! /usr/bin/env zsh
 
 dot="$(cd "$(dirname "$0")"; pwd)"
+args=$@
 source "$dot/radar-base.sh"
 
 if is_repo; then
   autoload colors && colors
   printf '%s' "%{$fg_bold[black]%} git:(%{$reset_color%}"
-  zsh_color_remote_commits
+  if show_remote_status $args; then
+    zsh_color_remote_commits
+  fi
   readable_branch_name
   zsh_color_local_commits
   printf '%s' "%{$fg_bold[black]%})%{$reset_color%}"
