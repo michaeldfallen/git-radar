eb9c08b9b0e05ca2aaf958e34979f6ae58af4e61
Michael Allen <michael@michaelallen.io>
2015-09-16 15:25:54 +0100

    switch prompt files to use new shared render functions

--- prompt.zsh
+++ prompt.zsh
@@ -10,10 +10,10 @@ if is_repo; then
   prepare_zsh_colors
   printf '%s' "%{$fg_bold[black]%} git:(%{$reset_color%}"
   if show_remote_status $args; then
-    zsh_color_remote_commits
+    color_remote_commits
   fi
-  zsh_readable_branch_name
-  zsh_color_local_commits
+  readable_branch_name
+  color_local_commits
   printf '%s' "%{$fg_bold[black]%})%{$reset_color%}"
-  zsh_color_changes_status
+  color_changes_status
 fi
