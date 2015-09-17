1d713f471fd77a49bc783ba0b313ebce9de223a5
Michael Allen <michael@michaelallen.io>
2015-09-17 14:21:38 +0100

    Surround non-printing chars in %{ and %}

--- radar-base.sh
+++ radar-base.sh
@@ -67,7 +67,7 @@ prepare_zsh_colors() {
   COLOR_BRANCH="%{${GIT_RADAR_COLOR_BRANCH:-$reset_color}%}"
   MASTER_SYMBOL="${GIT_RADAR_MASTER_SYMBOL:-"%{$reset_color%}$italic_m%{$reset_color%}"}"
 
-  PROMPT_FORMAT="${GIT_RADAR_FORMAT:-" $fg_bold[grey]git:($reset_color%{remote: }%{branch}%{ :local}$fg_bold[grey])$reset_color%{ :changes}"}"
+  PROMPT_FORMAT="${GIT_RADAR_FORMAT:-" %{$fg_bold[grey]%}git:(%{$reset_color%}%{remote: }%{branch}%{ :local}%{$fg_bold[grey]%})%{$reset_color%}%{ :changes}"}"
 
   RESET_COLOR_LOCAL="%{${GIT_RADAR_COLOR_LOCAL_RESET:-$reset_color}%}"
   RESET_COLOR_REMOTE="%{${GIT_RADAR_COLOR_REMOTE_RESET:-$reset_color}%}"
